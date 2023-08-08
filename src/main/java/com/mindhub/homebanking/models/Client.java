4de456bd27db3a57da679261fb4d18daf9bd71e8
Iara Baya <iarabayav@gmail.com>
2023-08-08 17:49:40 -0300

    feat: esqueleto del proyecto, cliente y cliente repo

---
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,58 @@
+package com.mindhub.homebanking.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Client {
+
+    //Atributos o propiedades
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name= "native", strategy = "native")
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    //Constructores
+    public Client() { }
+
+    public Client(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    //metodos
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+        return firstName + " " + lastName + " " + email ;
+    }
+}
