5c65f5b6d218c656063ddd6989f738ee52c10604
Iara Baya <iarabayav@gmail.com>
2023-08-30 18:05:25 -0300

    feat: endpoint de register en controller client

--- src/main/java/com/mindhub/homebanking/configurations/WebAuthorization.java
+++ src/main/java/com/mindhub/homebanking/configurations/WebAuthorization.java
@@ -1,9 +1,77 @@
-//package com.mindhub.homebanking.configurations;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//
-//@EnableWebSecurity
-//@Configuration
-//public class WebAuthorization extends W  {
-//}
+package com.mindhub.homebanking.configurations;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+@Configuration
+public class WebAuthorization   {
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((authz) -> authz
+                .requestMatchers(AntPathRequestMatcher.antMatcher("/index.html")).permitAll()
+                .requestMatchers(AntPathRequestMatcher.antMatcher("/web/**")).hasAuthority("CLIENT")
+                .requestMatchers(AntPathRequestMatcher.antMatcher("/rest/**")).hasAuthority("ADMIN")
+                .requestMatchers(AntPathRequestMatcher.antMatcher("/**")).hasAuthority("ADMIN")
+        );
+
+        // Me lo toma como error: cannot find symbol
+        // .antMatchers("/index.html", "/", "/web/css/**", "/web/js/**" ).permitAll();
+        // .antMatchers("/web/**").hasAuthority("CLIENT");
+        // .antMatchers("/rest/**").hasAuthority("ADMIN");
+        //  .antMatchers("/**").hasAuthority("ADMIN");
+
+        http.formLogin()
+                .usernameParameter("mail")
+                .passwordParameter("pwd")
+                .loginPage("/api/login");
+
+        http.logout().logoutUrl("/api/logout").logoutSuccessHandler((req,res,auth) -> res.setStatus(HttpServletResponse.SC_OK));
+
+        // turn off checking for CSRF tokens
+        http.csrf().disable();
+        //disabling frameOptions so h2-console can be accessed
+
+        http.headers().frameOptions().disable();
+
+        // if user is not authenticated, just send an authentication failure response
+
+        http.exceptionHandling().authenticationEntryPoint((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+        // if login is successful, just clear the flags asking for authentication
+
+        http.formLogin().successHandler((req, res, auth) -> clearAuthenticationAttributes(req));
+
+        // if login fails, just send an authentication failure response
+
+        http.formLogin().failureHandler((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+        // if logout is successful, just send a success response
+
+        http.logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
+        return http.build();
+    }
+
+
+    private void clearAuthenticationAttributes(HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+
+            session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+
+        }
+    }
+}
--- src/main/java/com/mindhub/homebanking/controllers/ClientController.java
+++ src/main/java/com/mindhub/homebanking/controllers/ClientController.java
@@ -4,9 +4,10 @@ import com.mindhub.homebanking.dtos.ClientDTO;
 import com.mindhub.homebanking.models.Client;
 import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,13 +17,34 @@ import java.util.stream.Collectors;
 public class ClientController {
 
     @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
     private ClientRepository clientRepository;
 
-    @RequestMapping("/clients")
+    @RequestMapping(path = "/clients", method = RequestMethod.GET)
     public List<ClientDTO> getClients(){
         List<Client> clientsList = clientRepository.findAll();
         return clientsList.stream().map(client -> new ClientDTO(client)).collect(Collectors.toList());
     }
+
+    @RequestMapping(path = "/clients", method = RequestMethod.POST)
+    public ResponseEntity<Object> register (@RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, @RequestParam String password) {
+        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty()) {
+
+            return new ResponseEntity<>("Missing data", HttpStatus.FORBIDDEN);
+        }
+
+        if (clientRepository.findByEmail(email) !=  null) {
+            return new ResponseEntity<>("Name already in use", HttpStatus.FORBIDDEN);
+        }
+
+        clientRepository.save(new Client(firstName, lastName, email, passwordEncoder.encode(password)));
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
+    }
+
     @RequestMapping("/clients/{id}")
     public ClientDTO getClient(@PathVariable Long id){
         Client client = clientRepository.findById(id).orElse(null);
